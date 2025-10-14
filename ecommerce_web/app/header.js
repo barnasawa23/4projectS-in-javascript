cc7732e2218a555c7fe9862b2e76ea1a466f41ec
landrykhamin <landryingabire605@gmail.com>
2025-10-14 12:46:44 +0200

    add a logo

--- ecommerce_web/app/header.js
+++ ecommerce_web/app/header.js
@@ -1,5 +1,7 @@
 import Cart from "@/components/Cart";
+import logo from "./favicon.ico"
 import Link from "next/link";
+import Image from "next/image";
 
 
 export default function Header() {
@@ -7,6 +9,7 @@ export default function Header() {
     <header>
       <div className="header-content">
         <Link href="/">
+          <Image src={logo} alt="Mu-Store Logo" width="30" height="30" />
           <h1>Mu-Store</h1>
         </Link>
         <h5 className="mid-text">Your one-stop shop for all things Mukaza!</h5>
